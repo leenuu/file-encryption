99c8b3558aeba554b84c5ba88017ed827eb0ad7a
leenuu <61344683+leenuu@users.noreply.github.com>
2021-08-23 15:15:08 +0900

    Update encryption_process.py

--- file_encryption/main/encryption_process.py
+++ file_encryption/main/encryption_process.py
@@ -1,5 +1,4 @@
 from PyQt5.QtCore import endl
-from cryptography import fernet
 from cryptography.fernet import Fernet
 import base64
 import json
@@ -8,7 +7,7 @@ class encryption_pro:
 
     def __init__(self):
         self.key_dir_path = list()
-        self.json_file_path = '' #path you want
+        self.json_file_path = 'C:\ProgramData\security\data.api'
         self.file_list = dict()
         self.data = b''
         self.file_key = b''
@@ -17,29 +16,36 @@ class encryption_pro:
         self.login_key_str = b''
 
     def login(self, path):
-        with open(path, 'r') as key_file:
-            data = key_file.read().split(',')
-            login_fernet = Fernet(data[0].encode('ascii'))
-            key = data[2].encode('ascii')
-            print(data) 
-
-        if self.login_str == login_fernet.decrypt(key):
-            self.json_key = data[0].encode('ascii')
-            self.file_key = data[1].encode('ascii')
-            self.login_key_str = key
-            self.load_key()
-            return 0
-
-        else:
-            return 1
+        try:
+            with open(path, 'r') as key_file:
+                data = key_file.read().split(',')
+                login_fernet = Fernet(data[0].encode('ascii'))
+                key = data[2].encode('ascii')
+                print(data) 
             
+            with open(self.json_file_path, 'r') as file:
+                file_data = file.read().split(',')
+                file_key = file_data[0].encode('ascii')
+
+            if self.login_str == login_fernet.decrypt(key) and self.login_str == login_fernet.decrypt(file_key):
+                self.json_key = data[0].encode('ascii')
+                self.file_key = data[1].encode('ascii')
+                self.login_key_str = login_fernet.encrypt(self.login_str)
+                return 0
+
+            else:
+                return 1
+                
+        except Exception as e:
+            print(e)
+
     def save_file_data(self):
         with open(self.json_file_path, 'w') as file:
             json_data = json.dumps(self.file_list, ensure_ascii=False, indent=4).encode('ascii')
             file_data = base64.b64encode(json_data)
             encrypt_data = self.json_fernet.encrypt(file_data)
             data = encrypt_data.decode('ascii')
-            
+            data = self.login_key_str.decode('ascii') + ',' + data
             file.write(data)
     
     def save_key(self):
@@ -47,7 +53,8 @@ class encryption_pro:
 
     def load_file_data(self):
         with open(self.json_file_path, 'r') as file:
-            file_data = file.read().encode('ascii')
+            data = file.read().split(',')
+            file_data = data[1].encode('ascii')
             decode_data = self.json_fernet.decrypt(file_data)
             json_data = base64.b64decode(decode_data).decode('ascii')
             self.file_list = json.loads(json_data)
@@ -79,15 +86,3 @@ class encryption_pro:
         base64_data = base64.b64decode(decode_data)
         with open(file_name, 'wb') as file:
             file.write(base64_data)
-
-
-
-    
-
-
-
-
-
-
-
-    
